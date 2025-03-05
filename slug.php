503a6e6ac250b523fb499a4dd6c863b079ae7540
neno012 <nenokusminto12@gmail.com>
2025-03-05 09:11:20 +0700

    Update slug.php

--- slug.php
+++ slug.php
@@ -1,6 +1,6 @@
 <?php 
 // $domain_web = "https://www.holidaymyboss.com/";
-$domain_web = "http://localhost:8080/local-performa/";
+$domain_web = "http://localhost:8080/Bossku/";
 $nama_web = "Holiday My Boss";
 
 ?>
