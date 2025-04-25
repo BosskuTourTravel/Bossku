819fea7a9849f9a38fea625c4bb71ab9aba21bc6
BosskuTourTravel <bosskutourandtravel@gmail.com>
2025-04-25 09:14:21 +0700

    Land Tour

--- search-cat-landtour.php
+++ search-cat-landtour.php
@@ -41,104 +41,106 @@ function get_pembulatan2($x)
 
 if ($_POST['cari'] !== "") {
 ?>
-    <div class="row" style="text-align: center; padding: 10px;">
-        <?php
-        $query_cat = "SELECT * FROM LT_add_Category where category='" . $_POST['cari'] . "' order by id ASC";
-        $rs_cat = mysqli_query($con, $query_cat);
-        while ($row_cat = mysqli_fetch_array($rs_cat)) {
-            $query2 = "SELECT * FROM  LT_itinerary2 where id='" . $row_cat['tour_id'] . "'";
-            $rs2 = mysqli_query($con, $query2);
-            $row2 = mysqli_fetch_array($rs2);
+    <div class="container mx-auto p-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <?php
+            $query_cat = "SELECT * FROM LT_add_Category where category='" . $_POST['cari'] . "' order by id ASC";
+            $rs_cat = mysqli_query($con, $query_cat);
+            while ($row_cat = mysqli_fetch_array($rs_cat)) {
+                $query2 = "SELECT * FROM  LT_itinerary2 where id='" . $row_cat['tour_id'] . "'";
+                $rs2 = mysqli_query($con, $query2);
+                $row2 = mysqli_fetch_array($rs2);
 
-            $query_bn2 = "SELECT * FROM LT_itinnew where kode='" . $row2['landtour'] . "' && agent_twn !='0' order by agent_twn ASC LIMIT 1";
-            $rs_bn2 = mysqli_query($con, $query_bn2);
-            $row_bn2 = mysqli_fetch_array($rs_bn2);
+                $query_bn2 = "SELECT * FROM LT_itinnew where kode='" . $row2['landtour'] . "' && agent_twn !='0' order by agent_twn ASC LIMIT 1";
+                $rs_bn2 = mysqli_query($con, $query_bn2);
+                $row_bn2 = mysqli_fetch_array($rs_bn2);
 
-            $data_twn = array(
-                "kurs" => $row_bn2['kurs'],
-                "nominal" => $row_bn2['agent_twn'],
-            );
-            $show_kurs_twn = get_kurs_manual2($data_twn);
-            $rs_kurs_twn = json_decode($show_kurs_twn, true);
-        
-            // var_dump($data_twn);
-            //  var_dump($rs_kurs_twn);
-        
-            $agent_twn = $rs_kurs_twn['data'];
+                $data_twn = array(
+                    "kurs" => $row_bn2['kurs'],
+                    "nominal" => $row_bn2['agent_twn'],
+                );
+                $show_kurs_twn = get_kurs_manual2($data_twn);
+                $rs_kurs_twn = json_decode($show_kurs_twn, true);
+                $agent_twn = $rs_kurs_twn['data'];
 
-            if ($row_bn2['agent_twn'] != "") {
-                $sql_profit2 = "SELECT * FROM LT_itin_profit_range_bossku where price1 <='" . $agent_twn . "' && price2 >='" . $agent_twn . "'";
-                $rs_profit2 = mysqli_query($con, $sql_profit2);
-                $row_profit2 = mysqli_fetch_array($rs_profit2);
+                if ($row_bn2['agent_twn'] != "") {
+                    $sql_profit2 = "SELECT * FROM LT_itin_profit_range_bossku where price1 <='" . $agent_twn . "' && price2 >='" . $agent_twn . "'";
+                    $rs_profit2 = mysqli_query($con, $sql_profit2);
+                    $row_profit2 = mysqli_fetch_array($rs_profit2);
 
-                $pr2 = 0;
-                if ($row_profit2['id'] != "") {
-                    $pr2 = $row_profit2['profit'];
-                } else {
-                    $pr2 = 5;
-                }
-                $ste2 = $row_profit2['staff_eks'];
-                $nom2 = $row_profit2['nominal'];
-                $atwn2 =  ($agent_twn * $pr2 / 100) + $agent_twn + $nom2;
-                $twn_sp2 = get_pembulatan2($atwn2);
-                $twn_rp2 = json_decode($twn_sp2, true);
-                $coret2 = $twn_rp2['value'] + 500000;
-        ?>
-                 <div class="col-xs-12 col-md-6 col-lg-4" style="padding: 5px 5px;">
-                    <a href="<?php  echo $domain_web ?>detail-landtour.php?id=<?php echo $row_bn2['id'] ?>&master=<?php echo $row2['id'] ?>" class="front-text" style="text-decoration: none; color:black">
-                        <div class="thumbnail">
-                            <img src="<?php  echo $domain_web ?>Admin/images/<?php echo $row2['gambar1'] ?>" class="img-fluid img-thumbnail2">
-                            <?php
-                            if ($row_bn2['statuss'] == 'E') {
-                            ?>
-                                <div class="top-left" style="color: red;">
-                                    EXPIRED
-                                </div>
-                            <?php
-                            }
-                            ?>
-                            <div class="card-body-tour">
-                                <div class="judul"><?php echo $row2['judul'] ?></div>
-                                <div style="color: gray; font-size: 8pt;"><?php echo $row_bn2['kota'] ?></div>
-                                <div style="color: gray; font-size: 8pt;">
-                                    <?php echo $row2['landtour'] ?> - <?php echo  $row_bn2['expired'] ?>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div style="color: gray; font-size: 8pt;">
-                                            <?php echo $pax_val2 . " Pax" ?>
-                                        </div>
+                    $pr2 = 0;
+                    if ($row_profit2['id'] != "") {
+                        $pr2 = $row_profit2['profit'];
+                    } else {
+                        $pr2 = 5;
+                    }
+                    $ste2 = $row_profit2['staff_eks'];
+                    $nom2 = $row_profit2['nominal'];
+                    $atwn2 =  ($agent_twn * $pr2 / 100) + $agent_twn + $nom2;
+                    $twn_sp2 = get_pembulatan2($atwn2);
+                    $twn_rp2 = json_decode($twn_sp2, true);
+                    $coret2 = $twn_rp2['value'] + 500000;
+                    $pax_u = "";
+                    $pax_b = "";
+                    if ($row_bn2['pax_u'] != 0) {
+                        $pax_u = "-" . $row_bn2['pax_u'];
+                    }
+                    if ($row_bn2['pax_b'] != 0) {
+                        $pax_b = "+" . $row_bn2['pax_b'];
+                    }
+                    $pax_val = $row_bn2['pax'] . $pax_u . $pax_b;
+            ?>
+                    <div class="bg-white shadow-md rounded-lg overflow-hidden flex flex-col">
+                        <a href="<?php echo $domain_web ?>detail-landtour.php?id=<?php echo $row_bn2['id'] ?>&master=<?php echo $row2['id'] ?>" class="no-underline text-black">
+                            <div class="relative">
+                                <img src="<?php echo $domain_web ?>Admin/images/<?php echo $row2['gambar1'] ?>" class="w-full object-cover h-48 pt-12">
+                                <?php if ($row_bn2['statuss'] == 'E') { ?>
+                                    <div class="top-2 left-2 text-red-500 font-bold text-sm bg-white bg-opacity-70 px-3 py-2 rounded">
+                                        EXPIRED
                                     </div>
-                                    <div class="col-md-6">
-                                        <div style="text-decoration: line-through; font-size: 9pt; color: darkmagenta; text-align: right;"> <?php echo "IDR " . number_format($coret2, 0, ",", ".") ?></div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-3" style="color: darkgreen;">
-                                        <i class="fa fa-building"></i>
-                                        <i class="fa fa-bus"></i>
-                                        <i class="fa fa-coffee"></i>
-                                        <div style="color: gray; font-size: 6pt;">SIC Everyday</div>
-                                    </div>
-                                    <div class="col-9">
-                                        <div class="card-body-price">
-                                            <div style="color: darkgreen;"><?php echo "IDR " . number_format($twn_rp2['value'], 0, ",", ".") ?></div>
-                                            <div style="color: gray; font-size: 6pt;">
-                                                Guarantee Departure (Start From 2pax) klik for price
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
+                                <?php } ?>
                             </div>
-                        </div>
-                    </a>
-                </div>
-        <?php
+
+                            <div class="px-4 py-2 flex-grow flex flex-col justify-between">
+                                <div class="mb-2">
+                                    <h3 class="font-bold text-lg"><?php echo $row2['judul'] ?></h3>
+                                    <p class="text-gray-500 text-xs"><?php echo $row_bn2['kota'] ?></p>
+                                    <p class="text-gray-500 text-sm"><?php echo $row2['landtour'] ?> - <?php echo $row_bn2['expired'] ?></p>
+                                </div>
+                                <div class="flex justify-between items-center">
+                                    <div class="text-gray-500 text-sm"><?php echo $pax_val . " Pax"; ?></div>
+                                    <div class="line-through text-purple-600 text-sm"><?php echo "IDR " . number_format($coret2, 0, ",", "."); ?></div>
+                                </div>
+
+                                <div class="flex items-start justify-between gap-2">
+                                    <div class="text-green-600 text-sm flex flex-col items-start">
+                                        <div>
+                                            <i class="fa fa-building mr-1"></i>
+                                            <i class="fa fa-bus mr-1"></i>
+                                            <i class="fa fa-coffee"></i>
+                                        </div>
+                                        <div class="text-gray-500 text-sm leading-tight">SIC Everyday</div>
+                                    </div>
+                                    <div class="flex-1 text-right">
+                                        <div class="text-green-600 text-lg font-semibold">
+                                            <?php echo "IDR " . number_format($twn_rp2['value'], 0, ",", ".") ?>
+                                        </div>
+                                        <div class="text-gray-600 text-sm">
+                                            Guarantee Departure (Start From 2pax) klik for price
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+            <?php
+                }
             }
-        }
-        ?>
+            ?>
+        </div>
     </div>
+
 <?php
 }
 ?>
